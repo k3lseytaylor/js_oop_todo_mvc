f777c5791644031805942c751879eaac4aab5b40
Daniel Paschal <daniel.paschal@learningfuze.com>
2017-07-03 16:40:13 -0700

    initial commit

--- app/controller.js
+++ app/controller.js
@@ -9,25 +9,9 @@ function TodoListController(options){
 		submitElement : options.submitElement
 	}
 	this.init = function(){
-		this.showAllItems();
+		
 	}
-	this.showAllItems = function(){
-		var items = this.model.readAll();
-		this.view.displayItemList(items);
-	}
-	this.handleSubmitClick = function(){
-		console.log('this is the controller submit click function');
-		var errors;
-		if((errors = this.view.checkValidInputs()).length>0){
-			this.view.displayModalMessage(errors.join('<br>'));
-		} else {
-			var itemDetails = this.view.getItemDetails();
-			debugger;
-			this.model.create(itemDetails);
-			this.view.displaySingleItem(itemDetails);
-		}
-	}
-	this.view = new TodoView(options, this.handleSubmitClick.bind(this));  //talking with the user
+	this.view = new TodoView();  //talking with the user
 	this.model = new TodoModel(); //talking with the server
 	//destination agnostic
 
